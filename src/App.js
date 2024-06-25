03f55cd25252d40bd55da08b74d231d0016ae2ae
Mamta Patil <mamtapatil44@gmail.com>
2024-06-25 13:50:42 -0400

    youtube latest changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const routes = createBrowserRouter([{
       element:<MainContainer />
     },
     {
-      path:'/',
+      path:'/watch',
       element:<WatchPage />
     }
   ]
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
-  return (
-    <div>WatchPage</div>
-  )
-}
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("v");
+  const dispatch = useDispatch();
 
-export default WatchPage
+  useEffect(() => {
+    dispatch(closeMenu(true));
+  }, []);
+  return (
+    <div className="flex w-full">
+      <div className="w-3/4">
+      <iframe
+        className="w-full mr-16 my-5 pr-2"
+        height="415"
+        src={"https://www.youtube.com/embed/"+ id}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+      </div>
+     
+      <div className="w-1/4">Recommendeations</div>
+    </div>
+  );
+};
+
+export default WatchPage;
